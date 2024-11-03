210c528f89e88fbc6613972aac0e6e864926e241
Gouri Panda <gouripanda4@gmail.com>
2024-11-03 18:08:37 +0530

    "Warning:(39, 26) The corresponding parameter in the supertype 'Migration' is named 'db'. This may cause problems when calling this function with named arguments."
    Renamed for unnecessary error in the future

--- app/src/main/java/com/javix/wordflipster/Database.kt
+++ app/src/main/java/com/javix/wordflipster/Database.kt
@@ -36,8 +36,8 @@ abstract class WordFlipsterDatabase : RoomDatabase() {
     }
 }
 val MIGRATION_1_2 = object : Migration(1, 2) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE challenges ADD COLUMN date INTEGER NOT NULL DEFAULT 0")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE challenges ADD COLUMN date INTEGER NOT NULL DEFAULT 0")
     }
 }
 
