fb3108eff996a54e2d847080c2fb0ccf3c809234
Gouri Panda <gouripanda4@gmail.com>
2024-11-20 22:15:08 +0530

    Fixed issue#10

--- app/src/main/java/com/javix/wordflipster/Utils.kt
+++ app/src/main/java/com/javix/wordflipster/Utils.kt
@@ -2,12 +2,12 @@ package com.javix.wordflipster
 
 fun calculateAverageTime(totalTimeMillis: Long, correctWords: Int): Double {
     return if (correctWords > 0) {
-        ((totalTimeMillis/ 1000)  / correctWords).toDouble()   // Convert to seconds
+        (totalTimeMillis.toDouble() / correctWords) / 1000 // Convert to seconds
     } else {
         0.0 // Avoid division by zero
     }
 }
 
 fun convertReadableTimeToString(time: Double) : String{
-    return String.format("%.5f", time)  // Format to 5 decimal places
+    return String.format("%.2f", time)  // Format to 5 decimal places
 }
